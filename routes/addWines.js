73c80f3a22f7be92fd23202a041cc23cb65182c0
demoulinc <demoulinc@hotmail.com>
2016-12-30 17:52:57 +0100

    be able to select color first draft

--- routes/addWines.js
+++ routes/addWines.js
@@ -4,21 +4,22 @@ var Wine = require('../public/javascripts/wine');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-   res.render('addWines', { title: 'Add new wine' });
+   var colorCollection = ['Red', 'white'];
+   res.render('addWines', { title: 'Add new wine', cols: colorCollection });
 });
 
 router.post('/', function(req, res) {
     
     var label = req.body.Label;
     var appellation = req.body.Appellation;
-    
+    var color = req.body.colors;
     var wine = new Wine();
     wine.label = label;
     wine.appellation = appellation;
 
     var app = require('../app');
     app.database.addWine(wine);
-    
+
     res.redirect('../');
 });
 
